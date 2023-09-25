743c509c89584aa487bcb1505217e42812a04f9e
arafatah <arafathowlader456@gmail.com>
2023-09-25 17:11:52 +0600

    data stored on local storage

--- src/Pages/CardDetails/CardDetails.jsx
+++ src/Pages/CardDetails/CardDetails.jsx
@@ -1,4 +1,7 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { saveStoredAmount } from "../../Components/Utility/LocalStorage";
 
 
 const CardDetails = () => {
@@ -6,6 +9,11 @@ const CardDetails = () => {
     const { id } = useParams();
     const idInt = parseInt(id);
     const card = cards.find(card => card.id === idInt);
+
+    const notify = () => {
+        toast("Thanks for helping poor by donating!");
+        saveStoredAmount(idInt);
+    }
 
     return (
         <div className="container mx-auto">
@@ -18,11 +26,13 @@ const CardDetails = () => {
 
                     
             </div>
-            <button className="px-6 py-4 rounded-sm text-white font-medium text-lg absolute top-[733px] left-[328px] bg-[#FF444A]">Donate {card.price}</button>
+            <button onClick={notify} className="px-6 py-4 rounded-sm text-white font-medium text-lg absolute top-[733px] left-[328px] bg-[#FF444A]">Donate {card.price}</button>
+            <ToastContainer></ToastContainer>
             <div className="lg:ml-28 lg:mt-10 md:mt-5">
                 <p className="font-bold text-4xl mb-4">{card.title}</p>
                 <p className="font-normal text-base text-justify lg:w-[1320px]">{card.description}</p>
             </div>
+            
         </div>
     );
 };
--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { getDonationStoredAmount } from "../../Components/Utility/LocalStorage";
+import TotalDonation from "../TotalDonaion/TotalDonation";
 
 const Donation = () => {
+    const cards = useLoaderData();
+    const [totalDonation, setTotalDonation] = useState([]);
+
+    useEffect(() => {
+        const storedCardsIds = getDonationStoredAmount();
+        if (cards.length > 0) {
+            const totalDonation = cards.filter(card => storedCardsIds.includes(card.id));
+            setTotalDonation(totalDonation);
+            console.log(totalDonation);
+        }
+
+    }, [cards])
     return (
         <div>
-            Donation here
+            {
+                totalDonation.length
+            }
+            <div>
+                {
+                    totalDonation.map(card => <TotalDonation key={totalDonation.id} card={card}></TotalDonation>)
+                }
+            </div>
         </div>
     );
 };
